0f72f6749d553c7677f096f26b2854f7307013ff
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-01-14 00:11:33 -0600

    style: :sparkles: finished landing page

---
+++ src/config/features.js
@@ -0,0 +1,37 @@
+export const FEATURED_CARDS = [
+	{
+		title: 'Desk',
+		description:
+			'A good desk is an essential piece of equipment for any home office or workspace.',
+		icon: 'ic:sharp-table-restaurant',
+		href: 'category/desk',
+	},
+	{
+		title: 'Computing',
+		description:
+			'A high-performance computer is a must-have for any tech enthusiast. I currently use a MacBook Air for work and a custom-built PC for gaming and video editing.',
+		icon: 'ic:baseline-desktop-mac',
+		href: 'category/computing',
+	},
+	{
+		title: 'Videography',
+		description:
+			'For videography, I use a mirrorless camera that offers excellent image quality and a wide range of manual controls. ',
+		icon: 'ic:baseline-missed-video-call',
+		href: 'category/videography',
+	},
+	{
+		title: 'Gadgets',
+		description:
+			'I am always on the lookout for new and innovative gadgets that can make my life easier.',
+		icon: 'ic:round-watch',
+		href: 'category/gadgets',
+	},
+	{
+		title: 'Entertainment',
+		description:
+			'When it comes to entertainment, I like to have options o different consoles.',
+		icon: 'ic:baseline-videogame-asset',
+		href: 'category/entertainment',
+	},
+];
