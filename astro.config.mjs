f98daeb50c4364dd9c9901072cddb920504d6fea
Geovany <marlongeo1999@gmail.com>
2023-01-13 23:17:42 -0600

    feat: :package: added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()]
 });
