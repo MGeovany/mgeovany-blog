e9905bc4a37f03951949e8e9e3c2744b62ed259b
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-01-15 23:43:57 -0600

    feat: :art: completed about page

---
+++ src/config/aboutPictures.js
@@ -0,0 +1,26 @@
+export const PICTURES_GRID = [
+	{
+		src: 'assets/images/about/build.jpeg',
+		alt: 'orange building',
+	},
+	{
+		src: 'assets/images/about/nike.jpeg',
+		alt: 'orange nike',
+	},
+	{
+		src: 'assets/images/about/citylights.jpeg',
+		alt: 'purple city lights',
+	},
+	{
+		src: 'assets/images/about/movecar.jpeg',
+		alt: 'purple city',
+	},
+	{
+		src: 'assets/images/about/palm.jpeg',
+		alt: 'palm moon',
+	},
+	{
+		src: 'assets/images/about/moon.jpeg',
+		alt: 'corner moon',
+	},
+];
--- src/config/footer.js
+++ src/config/footer.js
@@ -1,16 +1,25 @@
 export const footerSocials = [
 	{
+		user: '@m-geovany',
+		name: 'VSCO',
+		url: 'https://vsco.co/m-geovany/gallery',
+		icon: 'cib:vsco',
+	},
+	{
+		user: '@m-geovany',
 		name: 'LinkedIn',
 		url: 'https://www.linkedin.com/in/m-geovany/',
 		icon: 'mdi:linkedin',
 	},
 	{
+		user: '@mgeovany',
 		name: 'Github',
 		url: 'https://github.com/mgeovany',
 		icon: 'mdi:github',
 	},
 
 	{
+		user: '@m_geovany_',
 		name: 'Instagram',
 		url: 'https://www.instagram.com/m_geovany_/',
 		icon: 'mdi:instagram',
